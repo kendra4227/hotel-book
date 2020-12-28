2b8fa7d7f926b4f96628b1ecb4e256fd9558ba35
Lakendria <kendra939@gmail.com>
2020-12-27 20:20:01 -0500

    set up components and routes

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                hello from Single rooms page
+            </div>
+        );
+    }
+}
+
+export default SingleRoom;
